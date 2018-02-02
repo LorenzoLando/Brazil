b16c96e4314da2709dba7cfc70aa8378ab41a9d0
Lorenzo <lorenzo.lando85@outlook.com>
2018-02-02 12:58:25 +0000

    first commitment

--- js/brazil.js
+++ js/brazil.js
@@ -1,7 +1,45 @@
-//$(document).ready(function(){
-//
-//});
+//function for google maps
 
-$("h1").click(function(){
-	$(this).hide();
-});
+function initMap() {
+        var uluru = {lat: 57.1445876, lng: -2.1033751};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
+
+//when scrolling down nav background rgb(0,0,128)
+
+$(window).on('scroll', function() {    
+      var scroll = $(window).scrollTop();
+
+      if (scroll >= 500) {
+        
+ 			$('.navbar').removeClass('background-top');
+
+ //when scrolling up nav background back to normal
+      } else {
+
+      		$('.navbar').addClass('background-top');
+      }
+ });
+
+
+
+
+
+
+
+
+
+//when scrolling down button and h1 and h1 p to disappear
+
+
+
+
+
+//when scrolling up button and h1 and h1 p to re-appear
